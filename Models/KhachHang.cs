2c025db8d2fb28e5421099ae4598e511a2f0f7ae
Công Tùng <nct30000@gmail.com>
2025-10-13 15:14:39 +0700

    Model Controller Extension

---
+++ Models/KhachHang.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NemeShop.Models
+{
+    [Table("KhachHang")]
+    public partial class KhachHang
+    {
+        [Key]
+        [Column("MaKH")] // QUAN TRỌNG: Map đúng tên cột
+        public int MaKh { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string HoTen { get; set; } = null!;
+
+        [StringLength(50)]
+        public string? Email { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string MatKhau { get; set; } = null!;
+
+        [StringLength(15)]
+        public string? DienThoai { get; set; }
+
+        [StringLength(200)]
+        public string? DiaChi { get; set; }
+
+        public bool TrangThai { get; set; }
+
+        public virtual ICollection<DanhGiaSanPham> DanhGiaSanPhams { get; set; } = new List<DanhGiaSanPham>();
+
+        public virtual ICollection<HoaDon> HoaDons { get; set; } = new List<HoaDon>();
+    }
+}
---
+++ Models/ProductWishlistDto.cs
@@ -0,0 +1,14 @@
+﻿namespace NemeShop.Models
+{
+    public class ProductWishlistDto
+    {
+        public int MaDanhGia { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string? Image { get; set; }
+        public string? Category { get; set; }
+        public DateTime AddedDate { get; set; }
+        public int Stock { get; set; }
+    }
+}
